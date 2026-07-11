8482ad5a38de488c31981e1e7918dc286890f135
Stevie Owen Donfack Wamba <swamba@uok.ac.rw>
2026-07-11 11:57:36 +0200

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         tailwindcss(), // This must come BEFORE laravel()
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/welcome.js','resources/js/auth.js','resources/js/dashboard.js','resources/js/login.js','resources/js/reader.js','resources/js/registration.js','resources/js/login.js'],
             refresh: true,
         }),
     ],
